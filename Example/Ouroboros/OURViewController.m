34e5534cfe715e1a3702b5321202a32cde2a181b
Draveness <stark.draven@gmail.com>
2015-11-04 00:00:13 +0800

    Fixed demo

--- Example/Ouroboros/OURViewController.m
+++ Example/Ouroboros/OURViewController.m
@@ -44,7 +44,7 @@
                          }];
     [yellowView our_pinWithConfigureBlock:^(Scale * _Nonnull scale) {
         scale.offset = self.view.our_width;
-        scale.function = OURAnimationFunctionEaseIn;
+        scale.function = OURAnimationFunctionLinear;
     }];
 //    [yellowView our_animateWithProperty:OURAnimationPropertyViewFrame
 //                         configureBlock:^(Scale *scale) {
